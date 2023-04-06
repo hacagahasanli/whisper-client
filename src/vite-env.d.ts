72d158eb3a5b03d4f35230d00c00ae4d85ad51d7
Hesenli-Hacaga <hacaga125+1@gmail.com>
2023-04-06 22:53:18 +0400

    Folder Structure Created

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+/// <reference types="vite-plugin-react" />
+
+//that is the just example
+declare global {
+    interface ImportMetaEnv {
+        API_URL: string;
+    }
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      'pages': path.resolve(__dirname, 'src/pages/index.ts'),
+      'utils': path.resolve(__dirname, 'src/utils/index.js'),
+      'styles': path.resolve(__dirname, 'src/styles/index.js'),
+    },
+  },
 })
