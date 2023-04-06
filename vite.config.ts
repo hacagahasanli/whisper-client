523be1d2b8c28c12f45f59d004e5724e795851fe
Hesenli-Hacaga <hacaga125+1@gmail.com>
2023-04-06 23:09:01 +0400

    Tsconfig.json and Vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,8 +8,17 @@ export default defineConfig({
   resolve: {
     alias: {
       'pages': path.resolve(__dirname, 'src/pages/index.ts'),
-      'utils': path.resolve(__dirname, 'src/utils/index.js'),
-      'styles': path.resolve(__dirname, 'src/styles/index.js'),
+      'components': path.resolve(__dirname, 'src/components/index.ts'),
+      'config': path.resolve(__dirname, 'src/config/index.ts'),
+      'constants': path.resolve(__dirname, 'src/constants/index.ts'),
+      'helpers': path.resolve(__dirname, 'src/helpers/index.ts'),
+      'hooks': path.resolve(__dirname, 'src/hooks/index.ts'),
+      'recoil': path.resolve(__dirname, 'src/recoil/index.ts'),
+      'services': path.resolve(__dirname, 'src/services/index.ts'),
+      'shared': path.resolve(__dirname, 'src/shared/index.ts'),
+      'utils': path.resolve(__dirname, 'src/utils/index.ts'),
+      'assets': path.resolve(__dirname, 'src/assets/index.ts'),
+      'styles': path.resolve(__dirname, 'src/styles/index.ts'),
     },
   },
 })
