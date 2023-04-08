3d5716b552936cf612d22adbc4340319d598832f
Hesenli-Hacaga <hacaga125+1@gmail.com>
2023-04-08 23:02:49 +0400

    ..

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -3,7 +3,7 @@ import { IAccessButton } from "./IButton"
 const AccessButton = ({ type = "button", text, onClick }: IAccessButton) => (
     <button
         {...{ type, onClick }}
-        className="w-full min-h-full px-20 bg-access-btn-bg text-2xl text-white"
+        className="w-full min-h-full px-20 bg-access-btn-bg text-4xl text-white"
     >
         {text}
     </button>
