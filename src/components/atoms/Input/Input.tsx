b53c835b2600ddfd08e42f17c3a59fd4f0b7d937
Hesenli-Hacaga <hacaga125+1@gmail.com>
2023-04-10 17:22:49 +0400

    InputHelpers

--- src/components/atoms/Input/Input.tsx
+++ src/components/atoms/Input/Input.tsx
@@ -1,39 +1,24 @@
 import { IInput } from "./IInput"
 import { IInputsByTypeObject } from "interfaces"
 import { INPUTS_BY_TYPE } from "../../../constants"
+import { getValueByType } from "./InputHelpers"
 
 export const Input = ({ type, isRow, orderId }: IInput) => {
     const { label, id, placeholder }: IInputsByTypeObject = INPUTS_BY_TYPE[type]
-    const textSize = getFontSize(type);
-    const crsType = getCursorType(type);
-    const textC = getTextColor(type);
-
-    const flexD = isRow ? "row" : "col";
+    const { textSize, crsType, textC, appcenone, flexDir } = getValueByType(type, isRow)
     const odr = orderId || "0";
 
     return (
         <div>
-            <div className={`flex flex-${flexD} w-full justify-center gap-[.5rem]`}>
+            <div className={`flex flex-${flexDir} w-full justify-center gap-[.5rem]`}>
                 <label className={`text-[${textSize}] cursor-${crsType} text-[${textC}] `} htmlFor={id}>{label}</label>
                 <input
-                    className={`border border-solid border-input-brd order-[${odr}] cursor-${crsType} rounded-[.25rem] py-[.7rem] px-[.7rem] w-full`}
+                    className={`border border-solid ${appcenone} border-input-brd order-[${odr}] cursor-${crsType} rounded-[.25rem] py-[.7rem] px-[.7rem] w-full`}
                     {...{ type, id, placeholder }}
                 />
             </div>
         </div>
 
     )
-}
-
-function getFontSize(type:string) {
-    return type === "checkbox" ? "1rem" : "1.2rem";
-}
-
-function getCursorType(type:string) {
-    return type === "checkbox" ? "pointer" : "auto";
-}
-
-function getTextColor(type:string) {
-    return type === "checkbox" ? "gray-600" : "";
 }
 
--- src/components/molecules/LoginForm/LoginForm.tsx
+++ src/components/molecules/LoginForm/LoginForm.tsx
@@ -2,9 +2,9 @@ import { Input, AccessButton } from 'components';
 
 export const LoginForm = () => {
     return (
-        <div className="w-1/2 min-h-screen ml-[8rem] flex flex-col justify-center items-center relative">
-            <div className="flex flex-col justify-center items-center gap-[4rem] min-w-[400px] max-w-[400px] mt-[5rem] absolute left-0 mb-[8.5rem]">
-                <span className="text-[#393939] text-[2.5rem] w-full text-start font-bold">Log In</span>
+        <div className="w-1/2 min-h-screen flex flex-col justify-center items-center relative">
+            <div className="flex flex-col ml-[8rem] justify-center items-center min-w-[400px] max-w-[400px] mt-[5rem] absolute left-0 mb-[8.5rem]">
+                <span className="text-[#393939] text-[2.5rem] w-full text-start font-bold mb-[2rem]">Log In</span>
                 <form className="w-full">
                     <div className="flex flex-col gap-[1.4rem]">
                         <Input type="email" />
@@ -16,7 +16,13 @@ export const LoginForm = () => {
                             <span className="text-default-bg cursor-pointer">Forget password?</span>
                         </div>
                     </div>
-                    <AccessButton type="submit" text='Log in' />
+                    <div >
+                        <AccessButton type="submit" text='Log in' />
+                        <div className='flex justify-center gap-[.5rem] items-center mt-[1rem] cursor-pointer'>
+                            <span className='text-[#32568A]'>Don't have account?</span>
+                            <span className='underline text-default-bg'>Sign up</span>
+                        </div>
+                    </div>
                 </form>
             </div>
         </div>
