50f40a6c0eb44f9930a0f44a49c0d1d864a338de
Hesenli-Hacaga <hacaga125+1@gmail.com>
2023-04-16 22:07:14 +0400

    ...

--- src/components/organisms/Home/Home.tsx
+++ src/components/organisms/Home/Home.tsx
@@ -6,9 +6,18 @@ export const Home = () => {
         <ContainerLayout>
             <section className="bg-default-bg px-[12rem] py-[3rem] block w-full h-screen bg-cover bg-center" >
                 <Navbar />
-                <div className="flex flex-col justify-center items-center mt-[5rem] mx-auto">
+                <div className="flex flex-col justify-center items-center mt-[4.5rem] mx-auto">
                     <span className="text-white text-[3.5rem] max-w-[55rem] text-center font-[800]">Best choice for storing and sharing most important files.</span>
                     <span className="text-gray-blue mt-[1.5rem]">Easy to use, reliable, private, and secure</span>
+                </div>
+            </section>
+            <section>
+                <div className="flex flex-col gap-[.1rem] mx-auto justify-center items-center">
+                    <div className="flex justify-center items-center text-[4rem] gap-[1rem] font-[700] mt-[6.5rem]">
+                        <span className="text-default-bg">How</span>
+                        <span className="text-[#272727]">It Works</span>
+                    </div>
+                    <span>Securely store, organize, and share all your files.</span>
                 </div>
             </section>
         </ContainerLayout>
--- src/components/templates/ContainerLayout/ContainerLayout.tsx
+++ src/components/templates/ContainerLayout/ContainerLayout.tsx
@@ -1,7 +1,7 @@
 import { IChildrenProps } from "interfaces";
 
 export const ContainerLayout = ({ children }: IChildrenProps) =>
-    <div className="w-full min-h-screen flex">
+    <div className="w-full min-h-screen flex relative">
         <div className="w-full min-h-screen mx-auto ">
             {children}
         </div>
