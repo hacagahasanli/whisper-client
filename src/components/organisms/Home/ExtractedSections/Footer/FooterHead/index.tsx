f7fe60ad924b0a8522b3e8ca2059071adbfc85fe
Hesenli-Hacaga <hacaga125+1@gmail.com>
2023-04-21 19:32:07 +0400

    ....

--- src/components/organisms/Home/ExtractedSections/Footer/FooterHead/index.tsx
+++ src/components/organisms/Home/ExtractedSections/Footer/FooterHead/index.tsx
@@ -6,7 +6,7 @@ export const FooterHead = memo(({ FOOTER_ICONS }: IFooterHeadProps) => {
     return (
         <div className="w-full betweenItemsCenter pb-[2rem] mb-[3rem] border-b-[.1rem] border-[#ebebeb]">
             <Icon name="logo_dark" />
-            <div className="flexCenterx2 gap-[2.6rem]">
+            <div className="flexCenterx2 gap-[3rem]">
                 {FOOTER_ICONS.map(({ id, name }: IFooterIcons) => <Icon key={id} {...{ name }} />)}
             </div>
         </div>
