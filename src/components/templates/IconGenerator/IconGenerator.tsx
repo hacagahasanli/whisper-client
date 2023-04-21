d5d8ba14a113ab0b4c868ecdd730b1cf27859867
Hesenli-Hacaga <hacaga125+1@gmail.com>
2023-04-21 17:58:28 +0400

    Logo Dark

--- src/components/templates/IconGenerator/IconGenerator.tsx
+++ src/components/templates/IconGenerator/IconGenerator.tsx
@@ -1,12 +1,18 @@
 import { FC } from "react"
 import { IIconProps, IIcons } from "./IIconGenerator"
-import { ArrowLeft, ArrowRight, CheckMark, CrossMark, Cup, Debug, Encryption, Filter, LogoLight, Plans, Security, Shap, Sharing } from "./Icons"
+import { ArrowLeft, LogoDark, ArrowRight, CheckMark, CrossMark, Cup, Debug, Encryption, Filter, LogoLight, Plans, Security, Shap, Sharing } from "./Icons"
 
 export const Icon: FC<IIconProps> = ({ name, method, color, ...rest }) => {
     const icons: IIcons = {
+        //login
         "arrow_left": ArrowLeft,
+        
+        //global
         "arrow_right": ArrowRight,
         "logo_light": LogoLight,
+        "logo_dark": LogoDark,
+
+        //home
         "home_security": Security,
         "home_debug": Debug,
         "home_filter": Filter,
