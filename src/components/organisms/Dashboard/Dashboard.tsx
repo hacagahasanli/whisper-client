060739584047610f5161e729b1bb06f2cb9cc567
Hesenli-Hacaga <hacaga125+1@gmail.com>
2023-05-15 22:24:53 +0400

    Dcouments Table Title

--- src/components/organisms/Dashboard/Dashboard.tsx
+++ src/components/organisms/Dashboard/Dashboard.tsx
@@ -9,20 +9,23 @@ export const Dashboard = () => {
                 <div className="flex">
                     <AsideSection />
                     <main className="w-full">
-                        <div className="h-[138px] w-full relative">
-                            <span className="font-bold">My files</span>
-                            <div className="flex">
-                                <span>
-                                    <Icon name="row_grid" />
-                                </span>
-                                <span>
-                                    <Icon name="column_grid" />
-                                </span>
+                        <div className="relative h-[152px] w-full text-[#252C32]">
+                            <div className="w-full pt-[2rem] pl-[3rem] pr-[5rem] flex space-between flex-col">
+                                <span className="font-bold text-[2.5rem]">My files</span>
+                                <div className="flex self-end max-w-max cursor-pointer">
+                                    <span className="p-[.4rem] bg-[#E5E9EB]">
+                                        <Icon name="column_grid" />
+                                    </span>
+                                    <span className="p-[.4rem] bg-[#F6F8F9]">
+                                        <Icon name="row_grid" />
+                                    </span>
+                                </div>
+                                <div className="flex gap-2">
+                                    <span className="cursor-pointer">All files</span>
+                                    <span>Recently</span>
+                                </div>
                             </div>
-                            <div>
-                                <span>All files</span>
-                                <span>Recently</span>
-                            </div>
+                            <span className="absolute bottom-0 w-full h-[.15rem] bg-[#E5E9EB]"></span>
                         </div>
                     </main>
                 </div>
